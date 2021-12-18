89ed06bd2736850d6f252eafd6f66d54193cbf1d
kemal1132 <kemaldemirel1132@gmail.com>
2021-12-18 12:02:18 +0300

    Started working on a React game of life, started writing about grid

---
+++ components/gameOfLife/gameOfLife.js
@@ -0,0 +1,47 @@
+//Game of Life
+import React from 'react';
+import styles from "./gameOfLife.module.css";
+
+export default class GameOfLife extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+    
+    render() {
+      let gridrowsize=5;
+      let gridcolsize=2;
+
+      //styling the grid
+      let columnStyle = "";
+      for(let length = 0; length < gridcolsize; length++){
+        columnStyle += "auto "
+      }
+      let dropzoneStyle = {
+        gridTemplateColumns: columnStyle
+      }
+      
+      
+      
+      
+      let grid = [];
+      
+      for(let height = 0; height < gridrowsize; height++){
+        grid[height] = [];
+        for(let length = 0; length < gridcolsize; length++){
+          grid[height][length] = <div className='inactive' key={height.toString()+length.toString()}>grid {height.toString()+length.toString()}</div>;
+        }
+      }
+    
+        
+      
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <div className={styles.grid} style={dropzoneStyle}>
+            {grid}
+          </div>
+        </div>
+      );
+    }
+  }
---
+++ pages/gameoflife.js
@@ -0,0 +1,7 @@
+import GameOfLife from "../components/gameOfLife/gameOfLife";
+
+const gamePage = () => {
+    return (<GameOfLife></GameOfLife>);
+}
+
+export default gamePage;
