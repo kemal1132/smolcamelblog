109f1d155a5cd326227cb800cd844fc8062f3f4d
kemal1132 <kemaldemirel1132@gmail.com>
2021-12-19 00:11:04 +0300

    Added grid column/row control to gameoflife controller, struggled to update the grid(Probably shouldn't handle it in render method), also Didn't create termination logic for resizing down. So a middle of the road commit

--- components/gameOfLife/gameOfLife.js
+++ components/gameOfLife/gameOfLife.js
@@ -1,19 +1,23 @@
 //Game of Life
 import React from 'react';
-import styles from "./gameOfLife.module.css";
+import Style from "./gameOfLife.module.css";
 
 export default class GameOfLife extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            grid: []
+            grid: [],
+           
         }
+
+        
     }
     
-    render() {
-      let gridrowsize=50;
-      let gridcolsize=50;
 
+
+    render() {
+      let gridrowsize=this.props.gridRowSize;
+      let gridcolsize=this.props.gridColumnSize;
       //styling the grid
       let columnStyle = "";
       for(let length = 0; length < gridcolsize; length++){
@@ -26,21 +30,27 @@ export default class GameOfLife extends React.Component {
       
       
       
-      let grid = [];
       
-      for(let height = 0; height < gridrowsize; height++){
-        grid[height] = [];
-        for(let length = 0; length < gridcolsize; length++){
-          grid[height][length] = <div className='inactive' key={height.toString()+length.toString()}></div>;
-        }
-      }
+      
+        //TD handling removal of cells when size is changed
+        //TD handling click on cell
+        //TD adding more control
+        //TD adding game of life update logic
+        let grid = this.state.grid;
     
+        for(let height = 0; height < gridrowsize; height++){
+          grid[height] = [];
+          for(let length = 0; length < gridcolsize; length++){
+            grid[height][length] = <div id={height.toString()+"-"+length.toString()} onClick={this.handleClick} className={Style.inactive} position="test" key={height.toString()+"-"+length.toString()+Style.inactive}></div>;
+          }
+        }
         
+   
       
       return (
         <div>
-          <h1>Hello, world!</h1>
-          <div className={styles.grid} style={dropzoneStyle}>
+          <div className={Style.grid} style={dropzoneStyle}>
+ 
             {grid}
           </div>
         </div>
--- components/gameOfLife/gameOfLifeController.js
+++ components/gameOfLife/gameOfLifeController.js
@@ -8,19 +8,43 @@ import Style from './gameOfLifeController.module.css';
 export default class GameOfLifeController extends React.Component {
     constructor(props){
         super(props);
-       
+        this.state ={gridColumnSize:4, gridRowSize:4};
+
+        this.handleGridColumnSizeChange = this.handleGridColumnSizeChange.bind(this);
+        this.handleGridRowSizeChange = this.handleGridRowSizeChange.bind(this);
     }
     
+    handleGridColumnSizeChange(event){
+      if(event.target.value < 0){
+        event.target.value = 0;
+      }
+        this.setState({gridColumnSize:event.target.value});
+    }
+
+    handleGridRowSizeChange(event){  
+      if(event.target.value < 0){
+        event.target.value = 0;
+      }
+        this.setState({gridRowSize:event.target.value});
+    }
+
     render() {
       
       return (
         <div>
           <div className={Style.container}>
             <div className={Style.controlPanel}>
-              <h1>Hello, world!</h1>
+              <label>
+                Grid amount of columns:
+                <input className={Style.input} type="number" value={this.state.gridColumnSize} onChange={this.handleGridColumnSizeChange}></input>
+              </label>
+              <label>
+                Grid amount of rows: 
+                <input className={Style.input} type="number" value={this.state.gridRowSize} onChange={this.handleGridRowSizeChange}></input>
+              </label>
             </div> 
             <div className={Style.controlPanelPlaceHolder}></div>
-            <GameOfLife></GameOfLife>
+            <GameOfLife gridColumnSize={this.state.gridColumnSize} gridRowSize={this.state.gridRowSize}></GameOfLife>
           </div>
         </div>
         
