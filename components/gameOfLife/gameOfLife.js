d35e75cf885b554135742a142ec5d45020f55699
kemal1132 <kemaldemirel1132@gmail.com>
2022-01-17 15:51:08 +0300

    Instead of using arrays for game of life, transformed it to use objects (in other words hashsets) to immensly simplfy the logic

--- components/gameOfLife/gameOfLife.js
+++ components/gameOfLife/gameOfLife.js
@@ -8,11 +8,10 @@ export default class GameOfLife extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            grid: [],
+            grid: {},
             
         }
-        this.refreshGrid = this.refreshGrid.bind(this);
-        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+  
         this.handleClick = this.handleClick.bind(this);
        
         
@@ -21,43 +20,16 @@ export default class GameOfLife extends React.Component {
     handleClick = (event) => {
         let target= event.target.id.split("-");
         let grid = cloneDeep(this.state.grid);
-        if(event.target.className.toString().includes("inactive")){
-          grid[target[0]][target[1]] = {className:Style.active};
+        if(event.target.id in grid){
+          grid[event.target.id] = {isActive: !grid[event.target.id].isActive};
         }else{
-          grid[target[0]][target[1]] = {className:Style.inactive};
+          grid[event.target.id] = {xCor:target[0], yCor:target[1], isActive:true};
         }
         this.setState({grid:grid});
 
     }
 
-    refreshGrid(){
-        //TD handling removal of cells when size is changed --Tick
-        //TD handling click on cell
-        //TD adding more control
-        //TD adding game of life update logic
-        if(this.state.grid.length < this.props.gridRowSize||this.state.grid[0].length < this.props.gridColumnSize){
-          let grid = cloneDeep(this.state.grid);;
-          for(let height = 0; height < this.props.gridRowSize; height++){
-            if(height>=this.state.grid.length){
-              grid[height] = [];   //Dont create a new array if it already exists
-            }
-            for(let length = 0; length < this.props.gridColumnSize; length++){
-                if(length>=grid[height].length||height>=this.state.grid.length){ //Dont reset the box if it already exists
-                  grid[height][length] = { className:Style.inactive};
-                }
-            }
-          }
-          this.setState({grid:grid}, console.log(this.state));
-          
-        }
-    }
-    componentDidMount() {
-      this.refreshGrid();
-    }
 
-    componentDidUpdate(props){
-        this.refreshGrid();
-    }
 
 
     render() {
@@ -70,13 +42,18 @@ export default class GameOfLife extends React.Component {
         gridTemplateRows: "repeat("+(gridrowsize-1)+", 100px) 1fr",
       }
       
-      let grid = this.state.grid.slice(0, gridrowsize);
-      for(let i=0; i<grid.length; i++){
-        grid[i] = grid[i].slice(0, gridcolsize);
-        for(let j=0; j<grid[i].length; j++){
-          grid[i][j] = <div id={i+"-"+j} key={i+"-"+j} className={grid[i][j].className} onClick={this.handleClick}></div>
+    
+      let grid = [];
+      for(let i = 0; i<gridrowsize; i++){
+        for(let j = 0; j<gridcolsize; j++){
+          let id = i+"-"+j;
+          let className = this.state.grid[id]?this.state.grid[id].isActive?Style.active:Style.inactive:Style.inactive;
+          let cell = <div id={id} key={id} className={className} onClick={this.handleClick}></div>
+          grid.push(cell);
         }
       }
+
+      
       
       
       return (
